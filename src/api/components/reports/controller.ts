a57f73fac9ab7c90318212379730aa16d23da45a
root <root@NEKONET-AR>
2024-05-31 00:05:30 -0300

    (update): add dockerfiles

--- src/api/components/reports/controller.ts
+++ src/api/components/reports/controller.ts
@@ -1,84 +1,104 @@
 import { IJoin } from './../../../interfaces/Ifunctions';
 import { ETypesJoin } from './../../../enums/EfunctMysql';
-import { Ipages, IWhereParams } from 'interfaces/Ifunctions';
-
-import { EConcatWhere, EModeWhere, ESelectFunct } from '../../../enums/EfunctMysql';
+import { IWhereParams } from 'interfaces/Ifunctions';
+import { EConcatWhere, EModeWhere } from '../../../enums/EfunctMysql';
 import { Tables, Columns } from '../../../enums/EtablesDB';
 import StoreType from '../../../store/mysql';
-import getPages from '../../../utils/getPages';
-import { NextFunction } from 'express';
-import fs from 'fs';
 
 export = (injectedStore: typeof StoreType) => {
-    let store = injectedStore;
+  let store = injectedStore;
 
-    const products = async (fromDate: string, toDate: string, pvId?: number, idProd?: number) => {
-        let filter: IWhereParams | undefined = undefined;
-        let filters: Array<IWhereParams> = [];
-        console.log('pvId :>> ', pvId);
-        if (pvId) {
-            filter = {
-                mode: EModeWhere.strict,
-                concat: EConcatWhere.none,
-                items: [
-                    { column: `${Tables.FACTURAS}.${Columns.facturas.pv}`, object: String(pvId) },
-                ]
-            };
-            filters.push(filter);
-        }
-
-        if (idProd) {
-            filter = {
-                mode: EModeWhere.strict,
-                concat: EConcatWhere.none,
-                items: [
-                    { column: `${Tables.DET_FACTURAS}.${Columns.detallesFact.id_prod}`, object: String(idProd) },
-                ]
-            };
-            filters.push(filter);
-        }
-
-        filter = {
-            mode: EModeWhere.higherEqual,
-            concat: EConcatWhere.none,
-            items: [
-                { column: `${Tables.FACTURAS}.${Columns.facturas.fecha}`, object: String(fromDate) },
-            ]
-        };
-        filters.push(filter);
-
-        filter = {
-            mode: EModeWhere.lessEqual,
-            concat: EConcatWhere.none,
-            items: [
-                { column: `${Tables.FACTURAS}.${Columns.facturas.fecha}`, object: String(toDate) },
-            ]
-        };
-        filters.push(filter);
-
-        const join: IJoin = {
-            table: Tables.DET_FACTURAS,
-            colJoin: Columns.detallesFact.fact_id,
-            colOrigin: Columns.facturas.id,
-            type: ETypesJoin.left
-        }
-
-        const group: Array<string> = [`${Tables.FACTURAS}.${Columns.facturas.fecha}`]
-
-
-        const data = await store.list(Tables.FACTURAS, [
-            `SUM(${Tables.DET_FACTURAS}.${Columns.detallesFact.total_prod}) AS totalVenta`,
-            `SUM(${Tables.DET_FACTURAS}.${Columns.detallesFact.total_costo}) AS totalCosto`,
-            `SUM(${Tables.DET_FACTURAS}.${Columns.detallesFact.total_prod} - ${Tables.DET_FACTURAS}.${Columns.detallesFact.total_costo}) AS totalGanancia`,
-            `COUNT(${Tables.DET_FACTURAS}.${Columns.detallesFact.id}) AS cantidad`,
-            `(${Tables.FACTURAS}.${Columns.facturas.fecha}) AS fecha`
-        ], filters, group, undefined, [join]);
-        return {
-            data
-        };
+  const products = async (
+    fromDate: string,
+    toDate: string,
+    pvId?: number,
+    idProd?: number,
+  ) => {
+    let filter: IWhereParams | undefined = undefined;
+    let filters: Array<IWhereParams> = [];
+    if (pvId) {
+      filter = {
+        mode: EModeWhere.strict,
+        concat: EConcatWhere.none,
+        items: [
+          {
+            column: `${Tables.FACTURAS}.${Columns.facturas.pv}`,
+            object: String(pvId),
+          },
+        ],
+      };
+      filters.push(filter);
     }
 
+    if (idProd) {
+      filter = {
+        mode: EModeWhere.strict,
+        concat: EConcatWhere.none,
+        items: [
+          {
+            column: `${Tables.DET_FACTURAS}.${Columns.detallesFact.id_prod}`,
+            object: String(idProd),
+          },
+        ],
+      };
+      filters.push(filter);
+    }
+
+    filter = {
+      mode: EModeWhere.higherEqual,
+      concat: EConcatWhere.none,
+      items: [
+        {
+          column: `${Tables.FACTURAS}.${Columns.facturas.fecha}`,
+          object: String(fromDate),
+        },
+      ],
+    };
+    filters.push(filter);
+
+    filter = {
+      mode: EModeWhere.lessEqual,
+      concat: EConcatWhere.none,
+      items: [
+        {
+          column: `${Tables.FACTURAS}.${Columns.facturas.fecha}`,
+          object: String(toDate),
+        },
+      ],
+    };
+    filters.push(filter);
+
+    const join: IJoin = {
+      table: Tables.DET_FACTURAS,
+      colJoin: Columns.detallesFact.fact_id,
+      colOrigin: Columns.facturas.id,
+      type: ETypesJoin.left,
+    };
+
+    const group: Array<string> = [
+      `${Tables.FACTURAS}.${Columns.facturas.fecha}`,
+    ];
+
+    const data = await store.list(
+      Tables.FACTURAS,
+      [
+        `SUM(${Tables.DET_FACTURAS}.${Columns.detallesFact.total_prod}) AS totalVenta`,
+        `SUM(${Tables.DET_FACTURAS}.${Columns.detallesFact.total_costo}) AS totalCosto`,
+        `SUM(${Tables.DET_FACTURAS}.${Columns.detallesFact.total_prod} - ${Tables.DET_FACTURAS}.${Columns.detallesFact.total_costo}) AS totalGanancia`,
+        `COUNT(${Tables.DET_FACTURAS}.${Columns.detallesFact.id}) AS cantidad`,
+        `(${Tables.FACTURAS}.${Columns.facturas.fecha}) AS fecha`,
+      ],
+      filters,
+      group,
+      undefined,
+      [join],
+    );
     return {
-        products
-    }
-}
+      data,
+    };
+  };
+
+  return {
+    products,
+  };
+};
--- src/utils/facturacion/middleFiscal.ts
+++ src/utils/facturacion/middleFiscal.ts
@@ -1,75 +1,86 @@
-import ControllerInvoices from "../../api/components/invoices"
-import { NextFunction, Request, Response } from "express"
-import { INewPV } from "interfaces/Irequests"
-import { IFactura } from "interfaces/Itables"
-import { AfipClass } from "./AfipClass"
+import ControllerInvoices from '../../api/components/invoices';
+import { NextFunction, Request, Response } from 'express';
+import { INewPV } from 'interfaces/Irequests';
+import { IFactura } from 'interfaces/Itables';
+import { AfipClass, CbteTipos } from './AfipClass';
 
 export const fiscalMiddle = () => {
-    const middleware = async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
+  const middleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      let asociado = null;
+      const pvData: INewPV = req.body.pvData;
+      const newFact: IFactura = req.body.newFact;
+      const dataFiscal:
+        | FactInscriptoProd
+        | FactInscriptoProdNC
+        | FactInscriptoServ
+        | FactInscriptoServNC
+        | FactMonotribProd
+        | FactMonotribProdNC
+        | FactMonotribServ
+        | FactMonotribServNC
+        | any = req.body.dataFiscal;
+      if (dataFiscal.CbtesAsoc) {
+        asociado = dataFiscal.CbtesAsoc;
         try {
-            let asociado = null
-            const pvData: INewPV = req.body.pvData
-            const newFact: IFactura = req.body.newFact
-            const dataFiscal: FactInscriptoProd |
-                FactInscriptoProdNC |
-                FactInscriptoServ |
-                FactInscriptoServNC |
-                FactMonotribProd |
-                FactMonotribProdNC |
-                FactMonotribServ |
-                FactMonotribServNC |
-                any = req.body.dataFiscal
-            if (dataFiscal.CbtesAsoc) {
-                asociado = dataFiscal.CbtesAsoc
-                try {
-                    if (Number(asociado[0].Cuit) === 0 && Number(newFact.n_doc_cliente) !== 0) {
-                        asociado[0].Cuit = newFact.n_doc_cliente
-                    } else {
-                        asociado[0] = {
-                            Tipo: asociado[0].Tipo,
-                            PtoVta: asociado[0].PtoVta,
-                            Nro: asociado[0].Nro
-                        }
-                    }
-                } catch (error) {
+          if (
+            Number(asociado[0].Cuit) === 0 &&
+            Number(newFact.n_doc_cliente) !== 0
+          ) {
+            asociado[0].Cuit = newFact.n_doc_cliente;
+          } else {
+            asociado[0] = {
+              Tipo: asociado[0].Tipo,
+              PtoVta: asociado[0].PtoVta,
+              Nro: asociado[0].Nro,
+            };
+          }
+        } catch (error) {}
+      }
 
-                }
+      if (newFact.fiscal) {
+        let certDir = 'drop_test.crt';
+        let keyDir = 'drop.key';
+        let entornoAlt = false;
 
-            }
+        certDir = pvData.cert_file || 'drop_test.crt';
+        keyDir = pvData.key_file || 'drop.key';
+        entornoAlt = true;
 
-            if (newFact.fiscal) {
-                let certDir = "drop_test.crt"
-                let keyDir = "drop.key"
-                let entornoAlt = false
-
-                certDir = pvData.cert_file || "drop_test.crt"
-                keyDir = pvData.key_file || "drop.key"
-                entornoAlt = true
-
-                const afip = new AfipClass(newFact.cuit_origen, certDir, keyDir, entornoAlt);
-                const newDataFiscal = await afip.newFact(dataFiscal);
-                req.body.dataFiscal = newDataFiscal.data
-                req.body.dataFiscal.CbteTipo = String(newFact.t_fact)
-                req.body.newFact.cbte = req.body.dataFiscal.CbteDesde
-                if (asociado) {
-                    req.body.dataFiscal.CbtesAsoc = asociado
-                }
-
-                next()
-            } else {
-                const lastInvoice = await ControllerInvoices.lastInvoice(pvData.id || 0, false, 0, false)
-                newFact.cbte
-                req.body.newFact.cbte = lastInvoice.lastInvoice + 1
-                next()
-            }
-        } catch (error) {
-            console.log('error :>> ', error);
-            next(new Error("Faltan datos o hay datos erroneos, controlelo!"))
+        const afip = new AfipClass(
+          newFact.cuit_origen,
+          certDir,
+          keyDir,
+          entornoAlt,
+        );
+        const newDataFiscal = await afip.newFact(dataFiscal);
+        req.body.dataFiscal = newDataFiscal.data;
+        req.body.dataFiscal.CbteTipo = String(newFact.t_fact);
+        req.body.newFact.cbte = req.body.dataFiscal.CbteDesde;
+        if (asociado) {
+          req.body.dataFiscal.CbtesAsoc = asociado;
         }
+
+        next();
+      } else {
+        const lastInvoice = await ControllerInvoices.lastInvoice(
+          pvData.id || 0,
+          false,
+          CbteTipos.X,
+          false,
+        );
+        newFact.cbte;
+        req.body.newFact.cbte = lastInvoice.lastInvoice + 1;
+        next();
+      }
+    } catch (error) {
+      console.log('error :>> ', error);
+      next(new Error('Faltan datos o hay datos erroneos, controlelo!'));
     }
-    return middleware
-}
+  };
+  return middleware;
+};
